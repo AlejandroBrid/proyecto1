8e95d299ce1feea0c3b4e9895b0ba2393427c97b
Enric Masdeu <masdeu1022@gmail.com>
2023-06-21 16:57:30 +0200

    Cambio html y js

--- js/script.js
+++ js/script.js
@@ -1,34 +1,63 @@
 "use strict";
+const palabras = ["MANZANA", "BANANA", "ELEFANTE", "casa", "PERRO"];
 
-const listaPalabras = ["manzana", "banana", "elefante", "casa", "perro"];
+//Seleccionar una palabra al azar de la lista
+let palabra = palabras[Math.floor(Math.random()*palabras.length)];
 
-let palabra = listaPalabras[Math.floor(Math.random() * listaPalabras.length)];
+// Variables de juego
+let letrasAdivinadas = [];
+let intentos = 6;
 
-let letrasAdivinadas = [""];
-let intentosRestantes = 6;
-// Elementos HTML
+
+// Elementos HTML 
 const palabraHtml = document.getElementById("palabra");
+const letrasBtn = document.querySelectorAll("#letras button");
+
+
 // Función para mostrar la palabra oculta
 function mostrarPalabra() {
-  let palabraOculta = "";
-  for (let i = 0; i < palabra.length; i++) {
+let palabraOculta = "";
+for (let i = 0; i < palabra.length; i++) {
     if (letrasAdivinadas.includes(palabra[i])) {
-      palabraOculta += palabra[i] + " ";
+    palabraOculta += palabra[i] + " ";
     } else {
-      palabraOculta += "_ ";
+    palabraOculta += "_ ";
     }
-  }
-  palabraHtml.textContent = palabraOculta;
 }
-//evento de escucha a cada botón para capturar el clic del jugador y procesar la letra
-const botonesLetra = document.querySelectorAll(".letra");
+palabraHtml.textContent = palabraOculta;
+}
 
-botonesLetra.forEach((boton) => {
-  boton.addEventListener("click", () => {
-    const letra = boton.textContent;
-    boton.disabled = true; //desactivar el boton usado
-    letrasAdivinadas.push(letra);
+//////////////////////////////////////////////
+
+
+
+function verificarLetra(letra) {
+    letra = letra.toUpperCase();// No se porque no me funciona con minusculas :'(
+
+    if (letrasAdivinadas.includes(letra)) {
+        return;
+    }
+
+    if (palabra.includes(letra)) {
+        letrasAdivinadas.push(letra);
+    } else {
+        intentos--;
+    }
+
     mostrarPalabra();
-    verificarJuego();
-  });
+
+    if (intentos === 0) {
+        console.log("¡Has perdido! La palabra era: " + palabra);
+    } else if (!palabraHtml.textContent.includes("_")) {
+        console.log("¡Has ganado! La palabra es: " + palabra);
+    }
+}
+
+mostrarPalabra();
+
+letrasBtn.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        const letra = btn.textContent;
+        verificarLetra(letra);
+    });
 });
